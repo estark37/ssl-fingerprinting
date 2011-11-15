622bd4d80e630238120a3ffd0e9d411ab625467c
Emily Stark <emilystark@30-9-254.wireless.csail.mit.edu>
2011-11-15 14:49:34 -0500

    raw packet traces -> feature vectors

---
+++ preprocess.py
@@ -0,0 +1,50 @@
+import os, sys, time, shutil, subprocess
+from dateutil import parser
+
+def main():
+    input_dir = sys.argv[1]
+    data_src = sys.argv[2]
+    input_files = os.listdir(input_dir)
+
+    print input_files
+
+    # Some place to put the tcpdump-d raw input files
+    try:
+        shutil.rmtree("tmp_preprocess")
+    except Error:
+        print "No directory to delete"
+    os.mkdir("tmp_preprocess")
+    
+    # Process raw packet traces
+    for f in input_files:
+        subprocess.call("tcpdump -r %s/%s > tmp_preprocess/%s"%(input_dir, f, f), shell=True)
+
+    # Parse packet trace to feature vector
+    for input in input_files:
+        feature_vector = []
+        f = open("tmp_preprocess/%s"%input)
+        start = None
+        for p in f:
+            fields = p.split(" ")
+            # time relative to first packet's time
+            t = parser.parse(fields[0])
+            if start is None:
+                start = t
+            tdelta = (t - start).total_seconds()
+
+            # direction of packet
+            src = fields[2]
+            forwards = 0
+            if src.find(data_src) != -1:
+                forwards = 1
+
+            # and length
+            length = int(fields[-1])
+
+            feature_vector = feature_vector + [tdelta, forwards, length]
+        print "Feature vector for %s (length %s)"%(input, len(feature_vector))
+        print feature_vector
+
+
+main()
+    
